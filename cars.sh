d74da36f4f53883a8981f7f13be57214dd4e4fe7
ryanking916 <ryanking@chapman.edu>
2020-12-09 16:24:12 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#cars.sh
+#Ryan King
+'''
+echo 'Type the number 1 to enter a new car, the number 2 to display the list of cars, or the number 3 to quit and exit the program'
+read -r X
+'''
+while [[ $X -ne 3 ]]
+do
+	echo 'Type the number 1 to enter a new car, the number 2 to display the list of cars, or the number 3 to quit and exit the program'
+	read -r X
+	case $X in
+		1) echo 'What year is the car?'
+		read -r YEAR
+		echo 'What is the make of the car?'
+		read -r MAKE
+		echo 'What is the model of the car?'
+		read -r MODEL
+		CAR=$YEAR:$MAKE:$MODEL
+		echo $CAR >> My_old_cars;;
+		2) sort My_old_cars;;
+		3) echo 'Goodbye';;
+		*) echo 'Something is wrong with your input. Please try again.';;
+	esac
+done
+		 
