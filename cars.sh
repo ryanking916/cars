0310799cc72f0fa9f82839a1d4a81c57f462616e
ryanking916 <69736893+ryanking916@users.noreply.github.com>
2020-12-09 16:45:15 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -1,23 +1,19 @@
 #! /bin/bash
 #cars.sh
 #Ryan King
-'''
-echo 'Type the number 1 to enter a new car, the number 2 to display the list of cars, or the number 3 to quit and exit the program'
-read -r X
-'''
-while [[ $X -ne 3 ]]
+while [[ "$X" -ne "3" ]]
 do
-	echo 'Type the number 1 to enter a new car, the number 2 to display the list of cars, or the number 3 to quit and exit the program'
+	echo "Type the number 1 to enter a new car, the number 2 to display the list of cars, or the number 3 to quit and exit the program"
 	read -r X
 	case $X in
-		1) echo 'What year is the car?'
+		1) echo "What year is the car?"
 		read -r YEAR
-		echo 'What is the make of the car?'
+		echo "What is the make of the car?"
 		read -r MAKE
-		echo 'What is the model of the car?'
+		echo "What is the model of the car?"
 		read -r MODEL
-		CAR=$YEAR:$MAKE:$MODEL
-		echo $CAR >> My_old_cars;;
+		"CAR=$YEAR:$MAKE:$MODEL"
+		echo "$CAR" >> My_old_cars;;
 		2) sort My_old_cars;;
 		3) echo 'Goodbye';;
 		*) echo 'Something is wrong with your input. Please try again.';;
